8983b4bbc3e769f78855ed4b42666c5b06d28a74
Christin Magdy <cmagdy@bebrand.tv>
2020-04-04 17:31:25 +0200

    Dropdown and Thanks Page Design

--- AngProject/src/app/Account/login/login.component.ts
+++ AngProject/src/app/Account/login/login.component.ts
@@ -1,32 +1,30 @@
-import { AuthService } from './../../services/auth.service';
-import { LoginModel } from './../../Models/login-model';
-import { Validators } from '@angular/forms';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { RegisterServiceService } from 'src/app/services/register-service.service';
-import { Router } from '@angular/router';
+import { AuthService } from "./../../services/auth.service";
+import { LoginModel } from "./../../Models/login-model";
+import { Validators } from "@angular/forms";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { RegisterServiceService } from "src/app/services/register-service.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-
   constructor(
     private fb: FormBuilder,
     private service: RegisterServiceService,
-    private route:Router,
+    private route: Router,
     private auth: AuthService
-  ) { }
+  ) {}
 
   public messageValidate = {
     email: {
-      required: "Email is required",
-
+      required: "Email is required"
     },
     password: {
-      required: "Password is Required",
+      required: "Password is Required"
     }
   };
 
@@ -35,15 +33,14 @@ export class LoginComponent implements OnInit {
   log: LoginModel;
   errorMessage: string;
 
-
   ngOnInit() {
-    this.message = '';
-    this.errorMessage = '';
+    this.message = "";
+    this.errorMessage = "";
 
     this.log = {
       email: "",
       password: "",
-      rememberMe:false
+      rememberMe: false
     };
 
     this.loginForm = this.fb.group({
@@ -51,29 +48,23 @@ export class LoginComponent implements OnInit {
       password: ["", Validators.required],
       rememberMe: false
     });
-
   }
-
 
   Login() {
     if (this.loginForm.valid) {
       this.validateLoginModel();
       this.service.UserLogin(this.log).subscribe(
-        success =>
-        {
+        success => {
           const rem = !!this.loginForm.value.rememberMe;
           const email = this.loginForm.value.email;
           this.auth.installStorage(rem, email);
-          this.route.navigate(['home']);
+          this.route.navigate(["home"]);
         },
-        err =>
-        {
-           this.errorMessage = err.error;
-
+        err => {
+          this.errorMessage = err.error;
         }
       );
     }
-
   }
   validateLoginModel() {
     this.log.email = this.loginForm.value.email;
